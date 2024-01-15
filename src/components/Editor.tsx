fc7e6ee9b422176be65fb099136e9560997600f8
Nour-Elhamshary <nour.elhamshary@outlook.com>
2024-01-16 00:30:54 +0200

    Added ability to switch between two modes

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,20 +2,34 @@ import { useState } from "react"
 import "./Editor.css"
 import MarkdownDisplay from "./MarkdownDisplay";
 
-export default function Editor(){
+//Making a prop to hold 
+interface EditorProps {
+
+}
+
+export default function Editor({mode}: {mode:number}){
     const [getContent, takeUserInput] = useState('');
     
     return (
         <div className="editorDiv">
-            <div className="textAreaDiv">
-            <textarea
-            value = {getContent}
-            onChange = {input => takeUserInput(input.target.value)}
-            />
-            </div>
-            <div className="output">
-                <MarkdownDisplay markdown={getContent}/>
-            </div>
+            {mode==0 && (
+                <div className="textAreaDiv">
+                <textarea
+                value = {getContent}
+                 onChange = {input => takeUserInput(input.target.value)}
+                />
+                </div>
+            )  
+            }
+            {
+                mode==1 && (
+                    <div className="output">
+                    <MarkdownDisplay markdown={getContent}/>
+                    </div>
+                )
+            }
+
+
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { useState } from 'react';
 import Editor from '../components/Editor';
 
 const Home: React.FC = () => {
+  /*Set the one usestate for editor mode. It's integer for enumeration purposes. 
+  On the list of modes to switch:
+
+  0 - Script
+  1 - Preview
+  
+  */
+  const [isInMode, setMode] = useState(0);
+
+  //Set the two events to handle clicks.
+  // const handleScriptClick = (clickEvent: Event) => {
+  //   setMode(0);
+  // }
+
+  // const handlePreviewClick = (clickEvent: Event) => {
+  //   setMode(1);
+  // }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>NoteSketchr (VERY WIP)</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={() => setMode(0)}>Script</IonButton>
+            <IonButton onClick={() => setMode(1)}>Preview</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -18,7 +41,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         {/* <ExploreContainer /> */}
-        <Editor></Editor>
+        <Editor mode={isInMode}></Editor>
       </IonContent>
     </IonPage>
   );
